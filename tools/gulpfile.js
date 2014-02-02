69370661ae28a35ee5d091ac28e1ed81d261d6df
Konstantin Tarkus <hello@tarkus.me>
2014-02-02 18:55:08 +0400

    Fix whitespaces in gulpfile.js

--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -5,7 +5,7 @@ var gulp  = require('gulp'),
     gutil = require('gulp-util');
 
 gulp.task('default', function() {
-	gutil.log('Gulp.js has been successfully installed!\n' +
-		      'For more information on how to configure it, please visit:\n' +
-		      'https://github.com/gulpjs/gulp');
+    gutil.log('Gulp.js has been successfully installed!\n' +
+              'For more information on how to configure it, please visit:\n' +
+              'https://github.com/gulpjs/gulp');
 });
